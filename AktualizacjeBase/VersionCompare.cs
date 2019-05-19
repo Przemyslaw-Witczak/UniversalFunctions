7c93769f31793aa21de6ded23fa5b45c791fde31
Przemek <witczakprzemyslaw@gmail.com>
2019-05-19 23:40:53 +0200

    Refaktoring..przenosiny klas

---
+++ AktualizacjeBase/VersionCompare.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aktualizacje
+{
+
+    /// <summary>
+    /// Komparator do obiektów klasy Wersja
+    /// </summary>
+    public static class VersionCompare
+    {
+        /// <summary>
+        /// Metoda zwraca wartość logiczną PRAWDA, jezeli klasa w parametrze A jest mniejsza lub równa klasie w parametrze B
+        /// </summary>
+        /// <param name="versionA">Obiekt klasy Wersja</param>
+        /// <param name="versionB">Obiekt klasy Wersja</param>
+        /// <returns></returns>
+        public static bool IsLessOrEqual(Wersja versionA, Wersja versionB)
+        {
+         
+            bool returned_value = false;
+            if (IsEqual(versionA, versionB))
+                return true;
+
+            byte i = 0;
+            bool poprzedniaMniejsza = false;
+            do
+            {
+                if (versionA.Elements[i] < versionB.Elements[i])
+                {
+                    returned_value = true;
+                    poprzedniaMniejsza = true;
+
+                }
+                else if (versionA.Elements[i] == versionB.Elements[i])
+                {
+                    returned_value = true;
+                    poprzedniaMniejsza = false;
+                }
+                else
+                {
+                    returned_value = poprzedniaMniejsza;
+                    break;
+                }
+                i++;
+            }
+            while (i < 4);
+
+            
+
+            return returned_value;
+        }
+
+        public static bool IsEqual(Wersja versionA, Wersja versionB)
+        {
+            return versionA.Number == versionB.Number;
+        }
+    }
+}
---
+++ AktualizacjeBase/Wersja.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Aktualizacje
+{
+    /// <summary>
+    /// Klasa odpowiadająca obiektowi wersji aplikacji, dll 
+    /// </summary>
+    public class Wersja
+    {
+        public int A;
+        public int B;
+        public int C;
+        public int D;
+
+        public int[] Elements = new int[4];
+        public string Number
+        {
+            set
+            {
+                try
+                {
+                    string[] elements = value.Split(new char[] { '.' }, 4);
+                    for (byte i = 0; i < 4; i++)
+                        Elements[i] = Convert.ToInt32(elements[i]);
+                    A = Convert.ToInt32(Elements[0]);
+                    B = Convert.ToInt32(Elements[1]);
+                    C = Convert.ToInt32(Elements[2]);
+                    D = Convert.ToInt32(Elements[3]);
+                    
+                }
+                catch
+                {
+                    throw new Exception($"Cannot convert from string {value} to class Wersja!");
+                }
+            }
+            get
+            {
+                return $"{A}.{B}.{C}.{D}";
+            }
+        }
+
+    }
+}
