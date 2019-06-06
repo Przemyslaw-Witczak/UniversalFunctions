5b8eba831a6daf9c7b5f19d83f6291499abf91c2
Przemek <witczakprzemyslaw@gmail.com>
2019-06-06 23:29:50 +0200

    Metody rozszerzające do serializacji list

--- FunkcjeUniwersalne/FunkcjeUniwersalne/MetodyRozszerzajace.cs
+++ FunkcjeUniwersalne/FunkcjeUniwersalne/MetodyRozszerzajace.cs
@@ -302,4 +302,31 @@ namespace MojeFunkcjeRozszerzajace
               
     }
 
+    public static class ListExtensions
+    {
+        public static string ToDelimitedString<T>(this IEnumerable<T> source)
+        {
+            return source.ToDelimitedString(x => x.ToString(),
+                CultureInfo.CurrentCulture.TextInfo.ListSeparator);
+        }
+
+        public static string ToDelimitedString<T>(
+            this IEnumerable<T> source, Func<T, string> converter)
+        {
+            return source.ToDelimitedString(converter,
+                CultureInfo.CurrentCulture.TextInfo.ListSeparator);
+        }
+
+        public static string ToDelimitedString<T>(
+            this IEnumerable<T> source, string separator)
+        {
+            return source.ToDelimitedString(x => x.ToString(), separator);
+        }
+
+        public static string ToDelimitedString<T>(this IEnumerable<T> source,
+            Func<T, string> converter, string separator)
+        {
+            return string.Join(separator, source.Select(converter).ToArray());
+        }
+    }
 }
