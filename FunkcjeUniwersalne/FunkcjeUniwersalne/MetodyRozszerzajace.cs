121355d1d6eeb951ed9ef238b4fdda8cb0b1e992
Przemek <witczakprzemyslaw@gmail.com>
2019-06-10 23:25:28 +0200

    Poprawki formatowania kodu

--- FunkcjeUniwersalne/FunkcjeUniwersalne/MetodyRozszerzajace.cs
+++ FunkcjeUniwersalne/FunkcjeUniwersalne/MetodyRozszerzajace.cs
@@ -306,25 +306,20 @@ namespace MojeFunkcjeRozszerzajace
     {
         public static string ToDelimitedString<T>(this IEnumerable<T> source)
         {
-            return source.ToDelimitedString(x => x.ToString(),
-                CultureInfo.CurrentCulture.TextInfo.ListSeparator);
+            return source.ToDelimitedString(x => x.ToString(), CultureInfo.CurrentCulture.TextInfo.ListSeparator);
         }
 
-        public static string ToDelimitedString<T>(
-            this IEnumerable<T> source, Func<T, string> converter)
+        public static string ToDelimitedString<T>(this IEnumerable<T> source, Func<T, string> converter)
         {
-            return source.ToDelimitedString(converter,
-                CultureInfo.CurrentCulture.TextInfo.ListSeparator);
+            return source.ToDelimitedString(converter, CultureInfo.CurrentCulture.TextInfo.ListSeparator);
         }
 
-        public static string ToDelimitedString<T>(
-            this IEnumerable<T> source, string separator)
+        public static string ToDelimitedString<T>(this IEnumerable<T> source, string separator)
         {
             return source.ToDelimitedString(x => x.ToString(), separator);
         }
 
-        public static string ToDelimitedString<T>(this IEnumerable<T> source,
-            Func<T, string> converter, string separator)
+        public static string ToDelimitedString<T>(this IEnumerable<T> source, Func<T, string> converter, string separator)
         {
             return string.Join(separator, source.Select(converter).ToArray());
         }
