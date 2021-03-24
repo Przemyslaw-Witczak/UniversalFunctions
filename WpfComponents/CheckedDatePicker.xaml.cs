4b5c249c64cbb14de0328b23d2c480c263471369
Przemek <witczakprzemyslaw@gmail.com>
2021-03-24 23:51:59 +0100

    Dodanie komponentu DatePicker z Checkboxem i interakcjami pomiędzy... Po zmianie daty, następuje zaznaczenie checkBox, jak w WindowsForms

---
+++ WpfComponents/CheckedDatePicker.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfComponents
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class CheckedDatePicker : UserControl
+    {
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(CheckedDatePicker), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty SelectedDateProperty =
+            DependencyProperty.Register("SelectedDate", typeof(DateTime), typeof(CheckedDatePicker), new FrameworkPropertyMetadata(DateTime.Now, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public CheckedDatePicker()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Czy zaznaczony checkBox przy dacie
+        /// </summary>
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        public DateTime SelectedDate
+        {
+            get { return (DateTime)GetValue(SelectedDateProperty); }
+            set 
+            {
+                if (SelectedDate != value)
+                {
+                    IsChecked = true;
+                    SetValue(SelectedDateProperty, value);
+                }
+            }
+        }
+
+        private void DatePicker_CalendarOpened(object sender, RoutedEventArgs e)
+        {
+            IsChecked = true;
+        }
+    }
+}
