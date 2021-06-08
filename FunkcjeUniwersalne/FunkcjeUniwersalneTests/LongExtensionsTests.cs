afb806df884ae3c51e68e5cdb124d16e767a2bdd
Przemek <witczakprzemyslaw@gmail.com>
2021-06-08 21:10:08 +0200

    Usunięcie niepotrzebnych usingów

--- FunkcjeUniwersalne/FunkcjeUniwersalneTests/LongExtensionsTests.cs
+++ FunkcjeUniwersalne/FunkcjeUniwersalneTests/LongExtensionsTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MojeFunkcjeRozszerzajace;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MojeFunkcjeUniwersalneNameSpace.Extensions;
 
 namespace MojeFunkcjeRozszerzajace.Tests
 {
