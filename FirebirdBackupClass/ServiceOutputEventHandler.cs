d8532e81a2613ffab660aba164b15936e0162053
Przemek <witczakprzemyslaw@gmail.com>
2023-10-10 23:24:03 +0200

    Added missing references to DowiForms, and update nuget reference for firebird dll used by firebird backup class

---
+++ FirebirdBackupClass/ServiceOutputEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using FirebirdSql.Data.Services;
+using System;
+
+namespace FirebirdBackup
+{
+    internal class ServiceOutputEventHandler
+    {
+        private Action<object, ServiceOutputEventArgs> serviceOutput;
+
+        public ServiceOutputEventHandler(Action<object, ServiceOutputEventArgs> serviceOutput)
+        {
+            this.serviceOutput = serviceOutput;
+        }
+    }
+}
